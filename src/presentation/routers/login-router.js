f3c1b4d5094decf0b05a630c51bb6d38c1d9848d
fernndes <ferndes.gabriel@hotmail.com>
2022-01-12 20:25:20 -0300

    ensure LoginROuter returns 200 when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -9,7 +9,6 @@ module.exports = class LoginRouter {
     if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
       return HttpResponse.serverError()
     }
-
     const { email, password } = httpRequest.body
     if (!email) {
       return HttpResponse.badRequest('email')
@@ -17,9 +16,10 @@ module.exports = class LoginRouter {
     if (!password) {
       return HttpResponse.badRequest('password')
     }
-
-    this.authUseCase.auth(email, password)
-
-    return HttpResponse.unauthorizedError()
+    const accessToken = this.authUseCase.auth(email, password)
+    if (!accessToken) {
+      return HttpResponse.unauthorizedError()
+    }
+    return HttpResponse.ok()
   }
 }
